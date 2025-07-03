bf368c82a6e364f9a9c0efcb83847ecf95bf2c5b
spitegod <iermakov2207@gmail.com>
2025-07-03 15:43:57 +0300

    Подготовлен файл со списками листов для парсера по ип

--- parsers/__init__.py
+++ parsers/__init__.py
@@ -2,27 +2,27 @@ from .firstsem import parse_sheet as parse_first_sem
 from .secondsem import parse_sheet as parse_second_sem
 from .edu_methodwork import parse_methodical_work as parse_methodical_w
 from .org_methodwork import parse_methodical_work as parse_org
-from .sci_researchwork import parse_methodical_work as parse_org
-from .contractwork import parse_methodical_work as parse_orgs
-from .sci_methodwork import parse_methodical_work as parse_orgz
-from .published_sciworks import parse_methodical_work as parse_p
-from .public_work import parse_methodical_work as parse_pub
-from .remarks import parse_methodical_work as parse_r
-from .raising import parse_methodical_work as parse_rs
-from .recommendation import parse_methodical_work as parse_re
+from .sci_researchwork import parse_methodical_work as parse_sci
+from .contractwork import parse_methodical_work as parse_contract
+from .sci_methodwork import parse_methodical_work as parse_sci_method
+from .published_sciworks import parse_methodical_work as parse_published
+from .public_work import parse_methodical_work as parse_public
+from .remarks import parse_methodical_work as parse_remarks
+from .raising import parse_methodical_work as parse_raising
+from .recommendation import parse_methodical_work as parse_recom
 
 sheet_parsers = {
-    #"1. У.Р. Первый семестр": parse_first_sem,
-    #"1. У.Р. Второй семестр ": parse_second_sem,
-    #"2. Учебно-методическая работа": parse_methodical_w,
-    #"3. Организационно-методическая ": parse_org,
-    #"4. Научно-исследовательская раб": parse_org,
-    #"5. Участие в хоздоговорной НИР": parse_orgs,
-    #"6. Научно-методическая работа": parse_orgz,
-    #"7. Перечень публикаций и трудов": parse_p,
-    #"8. Общественная и воспитательна": parse_pub,
-    #"9. Замечания": parse_r,
-    #"Повышение квалификации": parse_rs,
-    "Рекомендация кафедры ": parse_re,
+    "1. У.Р. Первый семестр": parse_first_sem,
+    "1. У.Р. Второй семестр ": parse_second_sem,
+    "2. Учебно-методическая работа": parse_methodical_w,
+    "3. Организационно-методическая ": parse_org,
+    "4. Научно-исследовательская раб": parse_sci,
+    "5. Участие в хоздоговорной НИР": parse_contract,
+    "6. Научно-методическая работа": parse_sci_method,
+    "7. Перечень публикаций и трудов": parse_published,
+    "8. Общественная и воспитательна": parse_public,
+    "9. Замечания": parse_remarks,
+    "Повышение квалификации": parse_raising,
+    "Рекомендация кафедры ": parse_recom,
 
 }
